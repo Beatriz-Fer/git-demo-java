b1aceced8f2faa24e406dbc96a6d8e7c06970f7b
Beatriz <beatriz.ferreira@dell.com>
2023-06-08 20:21:50 +0100

    implementing changes

--- src/main/java/com/dell/App.java
+++ src/main/java/com/dell/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Learning Git using Java Project");
+        System.out.println("Learning Git using Java Project2");
     }
 }
